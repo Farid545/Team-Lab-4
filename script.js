1ecc5faf518e36367bfbf7116bbe71ff3216f873
Yaroslav Svidersky <megayara12@gmail.com>
2024-06-07 22:09:37 +0300

    added template files

---
+++ script.js
@@ -0,0 +1,8 @@
+const wrapper = document.querySelector(".wrapper"),
+    inputPart = wrapper.querySelector(".input-part"),
+    infoTxt = inputPart.querySelector(".info-txt"),
+    inputField = inputPart.querySelector("input"),
+    locationBtn = inputPart.querySelector("button")
+wIcon = document.querySelector(".weather-part img")
+arrowBack = document.querySelector("header i")
+let api;
